47261074971ae3b2a2ec8e323ee4643f93d18988
gi-a-dias <giovannaalmeidasdias@gmail.com>
2025-09-01 20:27:02 -0300

    organização

--- Funcionario.py
+++
@@ -1,9 +0,0 @@
-class Funcionario:
-    def __init__(self, nome, idade, salario_base, cargo, senha):
-        self.nome = nome
-        self.idade = idade
-        self.salario_base = salario_base
-        self.cargo = cargo
-        self.__senha = senha
-
-    
